2e4d995c4a53c9c354528ea772914cf8db6c9c25
Dylan Panepucci <dylanpanepucci@Mac.lan>
2025-02-16 18:16:33 -0500

    imports and main page set up complete. editing contents of tabs

--- Develop/src/main.jsx
+++ Develop/src/main.jsx
@@ -1,10 +1,39 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import ReactDOM from 'react-dom/client'
+// Bringing in the required imports from 'react-router-dom' to set up application routing behavior
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
+import App from './App';
+import Error from './Pages.jsx/Error';
+import Contact from './Pages.jsx/Contact';
+import About from './Pages.jsx/About';
+import Portfolio from './Pages.jsx/Portfolio';
+import Resume from './Pages.jsx/Resume';
+
+// Define the accessible routes, and which components respond to which URL
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <About />,
+      },{
+        path: 'Portfolio',
+        element: <Portfolio />,
+      },{
+        path: '/Contact',
+        element: <Contact />,
+      },{
+        path: 'Resume',
+        element: <Resume />,
+      }
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+);
