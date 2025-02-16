c8faed6016f3434f010705ffc9950890fc830a07
Dylan Panepucci <dylanpanepucci@Mac.lan>
2025-02-16 09:59:57 -0500

    Made changes to error page, added photo for use in future

---
+++ Develop/src/Pages.jsx/Error.jsx
@@ -0,0 +1,18 @@
+import { useRouteError } from 'react-router-dom';
+
+
+export default function ErrorPage() {
+  const error = useRouteError();
+  console.error(error);
+
+  return (
+    <div id="error-page">
+      <h1>Oops!</h1>
+      <img src="../../public/img/error_golf_page.jpg" alt="Error Page"/>
+      <p>We can't seem to find the page you're looking for.</p>
+      <p>
+        <i>{error.statusText || error.message}</i>
+      </p>
+    </div>
+  );
+}
