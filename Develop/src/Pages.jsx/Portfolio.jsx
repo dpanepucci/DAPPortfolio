35c6565679b1891521f415a2733602c8ac725257
Dylan Panepucci <dylanpanepucci@Mac.lan>
2025-02-20 13:48:38 -0500

    styling changes to portfolio page, added images for projects

--- Develop/src/Pages.jsx/Portfolio.jsx
+++ Develop/src/Pages.jsx/Portfolio.jsx
@@ -2,11 +2,28 @@ import '../styles/PagesStyle.css';
 
 export default function Portfolio() {
     return (
+      <div>
         <div>
           <h1 className="h1">Portfolio</h1>
           <p className="main-para">
-        SHOW OFF PROJECT YOU HAVE DONE
+        Top Projects
           </p>
         </div>
+        <div className='projectOne'>
+          <h2 className='project-name'>SQL Employee Tracker</h2>
+          <p className='project-description'>Opti-Tracker is a CLI that utilizes SQL, Postgres and Javascript to manage and adapt employee information.</p>
+          <a className='project-link' href="LINK TO PROJECT"> Opti-Tracker</a>
+        </div>
+        <div className='projectTwo'>
+          <h2 className='project-name'>README Generator</h2>
+          <p className='project-description'>SpeedyREADME allows users to generate a personalized README quickly, straight from the command line!</p>
+          <a className='project-link' href="LINK TO PROJECT"> SpeedyREADME</a>
+        </div>
+        <div className='projectThree'>
+          <h2 className='project-name'>Car Builder</h2>
+          <p className='project-description'>Command Line Cars is a fun and exciting CLI that lets user build and maneuver customized cars however they like!</p>
+          <a className='project-link' href="LINK TO PROJECT"> Command Line Cars</a>
+        </div>
+      </div>
       );
     }
