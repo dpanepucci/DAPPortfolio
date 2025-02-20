4b459ae7805e4d699eff93ed6a28d6de89f0b966
Dylan Panepucci <dylanpanepucci@Mac.lan>
2025-02-20 10:47:29 -0500

    working on Contact Form

--- Develop/src/Pages.jsx/Contact.jsx
+++ Develop/src/Pages.jsx/Contact.jsx
@@ -5,8 +5,14 @@ export default function Contact() {
       <div>
         <h1 className="h1">Contact Page</h1>
         <p className="main-para">
-        FORM WILL GO HERE
+        Let’s connect to discuss how we can shape the future of technology and healthcare together!
         </p>
+        <form id='form'>
+          <input type='input' placeholder='Name'/>
+          <input type='input' placeholder='Email'/>
+          <input className='message' type='input' placeholder='Message'/>
+          <button className='submit'>Submit</button>
+        </form>
       </div>
     );
   }
