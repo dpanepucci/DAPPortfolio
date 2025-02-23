bd2d8127cca1229789cb2ed6bf50ff942a97f40c
Dylan Panepucci <dylanpanepucci@Mac.lan>
2025-02-23 11:12:32 -0500

    missed pathway update

--- Develop/src/main.jsx
+++ Develop/src/main.jsx
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 
-import App from './App';
+import App from './App.jsx';
 import Error from './page-components/Error.jsx';
 import Contact from './page-components/Contact.jsx';
 import About from './page-components/About.jsx';
